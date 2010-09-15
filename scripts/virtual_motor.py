f1704fb6978004af6a66642b8d2f1cb2ca01b519
Andrew Straw <strawman@astraw.com>
2010-09-15 14:59:16 -0700

    extract motor virtualization to own module

---
+++ scripts/virtual_motor.py
@@ -0,0 +1,104 @@
+import roslib
+roslib.load_manifest('stage_tf')
+import rospy
+from stage.msg import StageCommands
+import numpy as np
+import time
+from stage.srv import Stage_StateRequest, Stage_State
+import subprocess
+
+# This is hardcoded in the USB device that sends commands.
+VELOCITY_STEP_DUR = 0.016
+
+class VirtualMotor(object):
+    def __init__(self):
+        self._publisher = rospy.Publisher('/Stage/Commands',
+                                          StageCommands,
+                                          tcp_nodelay=True,
+                                          latch=True,
+                                          )
+        self._get_stage_state_proxy = rospy.ServiceProxy('get_stage_state',
+                                                         Stage_State)
+        self.goto_blocking = self.goto_blocking_ros
+        #self.goto_blocking = self.goto_blocking_subprocess
+        self._last_usb = -np.inf
+
+    def goto_blocking_subprocess(self,x,y,dur=3.0):
+        response = self.get_stage_state()
+        print response.x, response.y
+
+        dx = x-response.x
+        dy = y-response.y
+
+        vx = dx/dur
+        vy = dy/dur
+
+        cmd = 'rostopic pub -1 /Stage/Commands stage/StageCommands -- True False False [%f] [%f] [%f] [%f]'%(x,y,vx,vy)
+        tstart=time.time()
+        subprocess.check_call(cmd,shell=True)
+        while 1:
+            now = time.time()
+            if (now-tstart) >= dur:
+                break
+            sleep_dur = (tstart+dur)-now
+            time.sleep(sleep_dur)
+        response = self.get_stage_state()
+        print response.x, response.y
+
+    def goto_blocking_ros(self,x,y,dur=1.0,xtol=1.0,ytol=1.0):
+        """goto position x,y, in dur seconds"""
+        if 1:
+            response = self.get_stage_state()
+            dx = x-response.x
+            dy = y-response.y
+
+            vx = dx/dur
+            vy = dy/dur
+
+            vel_mag = abs(np.sqrt( vx**2 + vy**2))
+
+            n_steps = int(round(dur/ VELOCITY_STEP_DUR))
+            n_steps = max(1,n_steps)
+
+            if 1:
+                # in position mode, we just do one step
+                n_steps=1
+
+            msg = StageCommands()
+
+            msg.x_position = [x]*n_steps
+            msg.y_position = [y]*n_steps
+            msg.x_velocity = []
+            msg.y_velocity = []
+            msg.velocity_magnitude = [vel_mag]*n_steps
+            if vel_mag != 0.0:
+                self.publish(msg)
+        while 1:
+            response = self.get_stage_state()
+            if ((abs(x-response.x) < xtol) and
+                (abs(y-response.y) < ytol)):
+                break
+        time.sleep(0.5)
+
+    # All this stuff is so we don't confuse the motor device by too
+    # frequent commands.
+    def _throttle_usb(self):
+        self._last_usb = -np.inf
+        now = time.time()
+        dur = now-self._last_usb
+        minimum_usb_cycle_duration = 0.05 # only hit ROS this often
+        while 1:
+            if dur > minimum_usb_cycle_duration:
+                break
+            target = (self._last_usb+minimum_usb_cycle_duration)-now
+            time.sleep(target)
+            now = time.time()
+        self._last_usb = now
+
+    def publish(self,msg):
+        self._throttle_usb()
+        self._publisher.publish(msg)
+
+    def get_stage_state(self):
+        self._throttle_usb()
+        return self._get_stage_state_proxy()
